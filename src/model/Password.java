d7334fad20cfd86889845848c484172a7576b66d
admin <baran.kaya@etu.iut-tlse3.fr>
2024-04-19 19:22:53 +0200

    Modification, Password should not have access to crypt method

--- src/model/Password.java
+++ src/model/Password.java
@@ -5,11 +5,10 @@ public class Password {
     private String username;
     protected String encryptedPassword;
     private PasswordManager passwordManager;
-    public Password(String website, String username, String password){
+    public Password(String website, String username, String encryptedPassword){
         this.website = website;
         this.username = username;
-        this.encryptedPassword = passwordManager.crypt(password);
+        this.encryptedPassword = encryptedPassword;
     }
-
 
 }
