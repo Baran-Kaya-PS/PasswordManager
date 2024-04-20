27c9c05f14f3d719249eb7aef873cdfddd269a26
admin <baran.kaya@etu.iut-tlse3.fr>
2024-04-20 19:42:01 +0200

    RSAPrivateKey getAlgorithm & getFormat

--- src/crypto/RSAPrivateKey.java
+++ src/crypto/RSAPrivateKey.java
@@ -5,12 +5,12 @@ import java.security.PrivateKey;
 public class RSAPrivateKey implements PrivateKey {
     @Override
     public String getAlgorithm() {
-        return "";
+        return "RSA"; // TODO Implement RSA Algorithm
     }
 
     @Override
     public String getFormat() {
-        return "";
+        return "PKCS#8";
     }
 
     @Override
