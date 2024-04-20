e6afd4fe3bb6d7a430b869869db227b2b1c6c934
admin <baran.kaya@etu.iut-tlse3.fr>
2024-04-20 19:35:36 +0200

    RSAKeyPairGenereator functions (not implemented yet)

--- src/crypto/RSAKeyPairGenereator.java
+++ src/crypto/RSAKeyPairGenereator.java
@@ -1,4 +1,27 @@
 package crypto;
 
+import java.math.BigInteger;
+import java.security.KeyPair;
+
 public class RSAKeyPairGenereator {
+    public RSAKeyPairGenereator(){}
+    public BigInteger generatePrimeNumbers(){
+        // (2 ^ 32) ^ Integer.MAX_VALUE
+        return new BigInteger("1");
+    }
+    public BigInteger computeModulus(){
+        return new BigInteger("1");
+    }
+    public BigInteger computeEulerTotient(){
+        return new BigInteger("1");
+    }
+    public BigInteger calculatePrivateExponent(){
+        return new BigInteger("1");
+    }
+    public BigInteger calculatePublicExponent(){
+        return new BigInteger("1");
+    }
+    public KeyPair generateKeyPair(){
+        return null;
+    }
 }
