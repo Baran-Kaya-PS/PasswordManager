0a0b5ad56fd7e56daff8e97fc795c27e08f8390d
admin <baran.kaya@etu.iut-tlse3.fr>
2024-04-20 22:25:57 +0200

    RSAKeyPairGenerator generatePrimeNumbers

--- src/crypto/RSAKeyPairGenereator.java
+++ src/crypto/RSAKeyPairGenereator.java
@@ -1,11 +1,27 @@
 package crypto;
 
+import crypto.utils.Pair;
+
 import java.math.BigInteger;
+import java.security.InvalidParameterException;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
 
 public class RSAKeyPairGenereator extends KeyPairGenerator {
-    public RSAKeyPairGenereator(){
+    public RSAKeyPairGenereator(int keysize){
         super("RSA");
+    }
+    @Override
+    public void initialize(int keysize, SecureRandom random) {
+        if (keysize < 2048){
+            throw new InvalidParameterException("key size must be greater than 2048");
+        }
+        super.initialize(keysize,random);
+    }
+    public Pair<BigInteger,BigInteger> generatePrimeNumbers(int keysize, SecureRandom random){
+        BigInteger p = BigInteger.probablePrime(keysize/2, random);
+        BigInteger q = BigInteger.probablePrime(keysize/2, random);
+        return new Pair<BigInteger,BigInteger>(p, q);
     }
 }
